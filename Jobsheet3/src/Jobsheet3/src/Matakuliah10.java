0069dac0b17b2ee426e052840f04910564a5771f
galihcandraa <galihcandrakirana17@gmail.com>
2026-02-24 11:20:33 +0700

    add percobaan 3.3

---
+++ Jobsheet3/src/Jobsheet3/src/Matakuliah10.java
@@ -0,0 +1,15 @@
+package Jobsheet3.src;
+
+public class Matakuliah10 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah10(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
