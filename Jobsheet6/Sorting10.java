497495c22ab85d829495b560e6296f0eb625590a
galihcandraa <galihcandrakirana17@gmail.com>
2026-03-31 10:31:18 +0700

    add praktikum 1 - insertion sort

--- Jobsheet6/Sorting10.java
+++ Jobsheet6/Sorting10.java
@@ -42,6 +42,19 @@ public class Sorting10 {
         }
     }
 
+    void insertionSort() {
+        for (int i = 0; i < jumData; i++) {
+            int temp = data[i];
+            int j = i - 1;
+
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = temp;
+        }
+    }
+
     void tampil() {
         for (int i = 0; i < jumData; i++) {
             System.out.print(data[i] + " ");
