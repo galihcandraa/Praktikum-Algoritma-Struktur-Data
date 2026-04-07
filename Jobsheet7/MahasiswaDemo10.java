cfaee80e21ae45920a2444d3d70d415f3d086457
galihcandraa <galihcandrakirana17@gmail.com>
2026-04-07 08:28:40 +0700

    add percobaan 7.1

---
+++ Jobsheet7/MahasiswaDemo10.java
@@ -0,0 +1,53 @@
+package Jobsheet7;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo10 {
+
+    static void inputData(Scanner sc, MahasiswaBerprestasi10 list, int jumMhs) {
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Data mahasiswa ke-" + (i + 1) + ": ");
+
+            Mahasiswa10 m = new Mahasiswa10();
+
+            System.out.print("Masukkan NIM   : ");
+            m.nim = sc.nextLine();
+            System.out.print("Masukkan Nama  : ");
+            m.nama = sc.nextLine();
+            System.out.print("Masukkan Kelas : ");
+            m.kelas = sc.nextLine();
+            System.out.print("Masukkan IPK   : ");
+            m.ipk = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("---------------------------");
+            list.tambah(m);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jumMhs = 5;
+
+        MahasiswaBerprestasi10 list = new MahasiswaBerprestasi10(jumMhs);
+
+        inputData(sc, list, jumMhs);
+
+        list.tampil();
+
+        // melakukan pencarian data Sequential
+        System.out.println("----------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("----------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearch(cari);
+        int pss = (int) posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+        sc.close();
+    }
+}
