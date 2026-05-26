bfae801951f524edd3b64f489760403c9c6aa0ad
galihcandraa <galihcandrakirana17@gmail.com>
2026-05-26 09:38:55 +0700

    add percobaan 14.1

---
+++ Jobsheet14/Node10.java
@@ -0,0 +1,14 @@
+package Jobsheet14;
+
+public class Node10 {
+    Mahasiswa10 mahasiswa;
+    Node10 left, right;
+
+    public Node10() {
+    }
+
+    public Node10(Mahasiswa10 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
